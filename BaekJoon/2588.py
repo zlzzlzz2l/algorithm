a298989e5aca201da7582b0f28b1f0376929dfa4
zlzzlzz2l <zlzzlzz2l@naver.com>
2021-05-21 20:56:29 +0900

    BaekJoon_곱셈

--- BaekJoon/2588.py
+++ BaekJoon/2588.py
@@ -1,13 +1,10 @@
-score = input()
-score = int(score)
+a = int(input())
+b = input()
 
-if score >= 90 and score <= 100:
-    print("A")
-elif score >= 80 and score <= 89:
-    print("B")
-elif score >= 70 and score <= 79:
-    print("C")
-elif score >= 60 and score <= 69:
-    print("D")
-else:
-    print("F")
+print(a * int(b[2]))
+
+print(a * int(b[1]))
+
+print(a * int(b[0]))
+
+print(a * int(b))
