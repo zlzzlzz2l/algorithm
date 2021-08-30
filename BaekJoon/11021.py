d233153addba23a6d5a0fcdb5bc83a16483e0e26
zlzzlzz2l <zlzzlzz2l@naver.com>
2021-08-31 02:03:32 +0900

    BaekJoon_A+B - 7

--- BaekJoon/11021.py
+++ BaekJoon/11021.py
@@ -4,4 +4,4 @@ num = int(input())
 
 for key in range(num):
     a, b = sys.stdin.readline().rstrip().split()
-    print("Case #%d: %s" % (key + 1, int(a) + int(b)))
+    print("Case #%d: %d" % (key + 1, int(a) + int(b)))
