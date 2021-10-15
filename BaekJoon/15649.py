22aac9d7114f9945a538eb3082a3f105802fa8ee
Ahyun Kim <zlzzlzz2l@naver.com>
2021-10-15 23:20:00 +0900

    BOJ_N과 M(1)

--- BaekJoon/15649.py
+++ BaekJoon/15649.py
@@ -1,10 +1,11 @@
-from itertools import permutations
+from itertools import permutations # 순열 라이브러리
 
-N, M = map(int, (input().split()))
+N, M = map(int, (input().split())) # N과 M 입력 받기
 
-data = [i for i in range(1, N+1)]
+data = [i for i in range(1, N+1)] # 1부터 N까지 데이터를 리스트화
 
-result = list(permutations(data, M))
+result = list(permutations(data, M)) # data 리스트 안에서 서로 다른 M개의 데이터 뽑기
 
 for i in result:
-    print(' '.join(map(str, i)))
+    print(i)
+    print(' '.join(map(str, i))) # 튜플 값을 문자열로 바꾼 뒤 문자열로 합친다. 튜플 -> 문자열
