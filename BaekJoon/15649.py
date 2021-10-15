0043d20b1c1b9d296698dcbaaff2f4dabcbe8df5
Ahyun Kim <zlzzlzz2l@naver.com>
2021-10-15 23:21:18 +0900

    BOJ_N과 M(1)

--- BaekJoon/15649.py
+++ BaekJoon/15649.py
@@ -7,5 +7,4 @@ data = [i for i in range(1, N+1)] # 1부터 N까지 데이터를 리스트화
 result = list(permutations(data, M)) # data 리스트 안에서 서로 다른 M개의 데이터 뽑기
 
 for i in result:
-    print(i)
     print(' '.join(map(str, i))) # 튜플 값을 문자열로 바꾼 뒤 문자열로 합친다. 튜플 -> 문자열
