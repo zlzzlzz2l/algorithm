8b739eef2ad7e3dade957986f1f456cace06d235
Ahyun Kim <zlzzlzz2l@naver.com>
2021-10-18 23:48:46 +0900

    BOJ_N과 M(4)

---
+++ BaekJoon/15652.py
@@ -0,0 +1,10 @@
+from itertools import combinations_with_replacement # 중복 조합 라이브러리
+
+N, M = map(int, (input().split())) # N과 M 입력 받기
+
+data = [i for i in range(1, N+1)] # 1부터 N까지 데이터를 리스트화
+
+result = list(combinations_with_replacement(data, M)) # data 리스트 안에서 서로 다른 M개의 데이터 뽑기 (중복 허용)
+
+for i in result:
+    print(' '.join(map(str, i))) # 튜플 값을 문자열로 바꾼 뒤 문자열로 합친다. 튜플 -> 문자열
