cd63d25a9ab3214f54e14238af9f15f952104f82
Ahyun Kim <zlzzlzz2l@naver.com>
2021-10-19 22:59:53 +0900

    BOJ_N과 M(6)

---
+++ BaekJoon/15655.py
@@ -0,0 +1,10 @@
+from itertools import combinations # 조합 라이브러리
+
+N, M = map(int, (input().split())) # N과 M 입력 받기
+
+data = sorted(list(map(int, input().split()))) # N개의 수 오름차순 리스트화
+
+result = list(combinations(data, M)) # data 리스트 안에서 순서에 상관없이 서로 다른 M개 선택
+
+for i in result:
+    print(' '.join(map(str, i))) # 튜플 값을 문자열로 바꾼 뒤 문자열로 합친다. 튜플 -> 문자열
