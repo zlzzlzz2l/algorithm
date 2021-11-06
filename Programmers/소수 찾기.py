9b226fc643ff9c4103ac1cd18f44c29fac79d8d2
Ahyun Kim <zlzzlzz2l@naver.com>
2021-11-06 22:32:25 +0900

    Programmers_연습문제_소수 찾기

--- Programmers/소수 찾기.py
+++ Programmers/소수 찾기.py
@@ -1,24 +1,16 @@
 def solution(n):
-    arr = [] # 2부터 n+1까지 나눴을 때 나누어 떨어지는 수 리스트
-    rm = [] # 1과 자기 자신을 제외한 수로 나눠지는 수 리스트
-
-    # 2부터 n+1까지 나눴을 때 나누어 떨어지는 수 arr 리스트에 추가
-    for i in range(2, n+1):
-        for j in range(2, n+1):
+    answer = 0
+    for i in range(2, n+1): # 입력값들 내에서 소수 찾기
+        cnt = 0
+        for j in range(2, i): # 2부터 자기 자신까지 반복
             if i % j == 0:
-                arr.append(i) # 1개 담긴 숫자는 소수
+                cnt += 1
+                break
+        if cnt == 0:
+            answer += 1
 
-    # 소수가 아닌 숫자 rm 리스트에 추가 및 중복 제거
-    for i in range(1, len(arr)-1):
-        if arr[i-1] == arr[i]:
-            rm.append(arr[i])
-        rm = set(rm)
-        rm = list(rm)
-
-    # 입력값에서 1과 rm의 길이를 빼면 소수 개수가 나옴
-    answer = n - 1 - len(rm)
     return answer
 
 # 입출력 예시
-n = 5
+n = 10
 print(solution(n))
