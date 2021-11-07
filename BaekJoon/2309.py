36974f7eb30ab9e2c7a68cbf4c5f09050a5ded0a
Ahyun Kim <zlzzlzz2l@naver.com>
2021-11-07 22:26:27 +0900

    BOJ_일곱 난쟁이

--- BaekJoon/2309.py
+++ BaekJoon/2309.py
@@ -1,11 +1,11 @@
-from itertools import permutations # 순열 라이브러리
+from itertools import combinations # 조합 라이브러리
 
 a = [] # inpurt값 담을 리스트
 for i in range(9):
     n = int(input()) # 9개의 값 입력
     a.append(n) # 입력된 값 리스트 추가
 
-result = list(permutations(a, 7)) # 원소 중에 7개 뽑기
+result = list(combinations(a, 7)) # 원소 중에 7개 뽑기
 
 for i in result: # 7개씩 뽑은 튜플로 반복
     if sum(i) == 100: # 해당 튜플 값이 100이라면
