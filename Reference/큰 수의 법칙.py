f4ce0c2f96254365bf3a32098b659cff979d2bc0
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-26 23:25:05 +0900

    Refactor: 그리디 큰 수의 법칙

--- Reference/큰 수의 법칙.py
+++ Reference/큰 수의 법칙.py
@@ -1,3 +1,4 @@
+# A1
 # 입력값 받기
 N, M, K = map(int, input().split())
 num = list(map(int, input().split()))
@@ -7,15 +8,46 @@ num.sort(reverse=True)
 
 # result: 더한 값, count: 더한 횟수
 result = 0
+first = num[0]
+second = num[1]
 count = 0
 
 for _ in range(M): # M만큼 반복
-    result += num[0] # 제일 큰 값을 더해준다.
+    result += first # 제일 큰 값을 더해준다.
     count += 1 # 횟수 증가
     if count % K == 0: # 횟수를 K로 나눴을 때의 나머지가 0이라면(큰 수를 K만큼 더했다는 뜻)
-        result += num[1] # 그 다음 큰 값을 더해준다
+        result += second # 그 다음 큰 값을 더해준다
         count += 1
     if count == M: # 횟수가 M과 같다면(숫자가 총 더해지는 횟수와 같기 때문에)
         break # 반복 탈출
 
 print(result)
+
+# A2
+# 입력값 받기
+# import time
+# st = time.time()
+#
+# N, M, K = map(int, input().split())
+# num = list(map(int, input().split()))
+#
+# # 내림차순 정렬
+# num.sort()
+# first = num[N-1]
+# second = num[N-2]
+#
+# result = 0
+#
+# while True:
+#     for i in range(K):
+#         if M == 0:
+#             break
+#         result += first
+#         M -= 1
+#     if M == 0:
+#         break
+#     result += second
+#     M -= 1
+# print(result)
+# et = time.time()
+# print("time: ", et-st)
