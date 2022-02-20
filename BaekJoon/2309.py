6d4b298770feb1171a841521896915c2272f9a77
Ahyun Kim <zlzzlzz2l@naver.com>
2022-02-20 18:21:45 +0900

    Solve: BOJ 일곱난쟁이
    기존 코드보다 깔끔하게 풀이하였습니다.

--- BaekJoon/2309.py
+++ BaekJoon/2309.py
@@ -1,17 +1,11 @@
 from itertools import combinations # 조합 라이브러리
 
-a = [] # inpurt값 담을 리스트
-for i in range(9):
-    n = int(input()) # 9개의 값 입력
-    a.append(n) # 입력된 값 리스트 추가
+heights = [int(input()) for _ in range(9)]
 
-result = list(combinations(a, 7)) # 원소 중에 7개 뽑기
-
-for i in result: # 7개씩 뽑은 튜플로 반복
+for i in combinations(heights, 7): # 7개씩 뽑은 튜플로 반복
     if sum(i) == 100: # 해당 튜플 값이 100이라면
-        i = sorted(i) # 튜플 오름차순 정렬
-        for s in range(len(i)):
-            print(i[s]) # 원소 하나씩 출력
+        for h in sorted(i):
+            print(h) # 원소 하나씩 출력
         break
     else:
         continue
