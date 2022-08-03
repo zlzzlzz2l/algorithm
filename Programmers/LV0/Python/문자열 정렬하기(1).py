ca1d01a705d2eeae56bb7b6e2b91aabe1961878e
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-08-03 14:26:51 +0900

    Solve: Programmers 코딩테스트 입문 문자열 정렬하기(1)

---
+++ Programmers/LV0/Python/문자열 정렬하기(1).py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = []
+    for s in my_string:
+        if s.isdigit():
+            answer.append(int(s))
+        else:
+            continue
+    return sorted(answer)
+
+print(solution("abcde0"))
