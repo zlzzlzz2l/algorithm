b561ad20142ccd2bd838198953003d708a884e44
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-09-05 10:30:21 +0900

    Solve: Programmers 코딩테스트 입문 배열 원소의 길이

---
+++ Programmers/LV0/Python/배열 원소의 길이.py
@@ -0,0 +1,9 @@
+def solution(strlist):
+    answer = []
+    for i in strlist:
+        answer.append(len(i))
+    return answer
+
+
+strlist = ["We", "are", "the", "world!"]
+print(solution(strlist))
