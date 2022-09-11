607e4ce981124ac0b6ddd2c26027a1edae3e3338
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-09-11 12:41:59 +0900

    Solve: Programmers 코딩테스트 입문 아이스 아메리카노

---
+++ Programmers/LV0/Python/아이스 아메리카노.py
@@ -0,0 +1,11 @@
+def solution(money):
+    answer = []
+    cup = money // 5500
+    change = money % 5500
+    answer.append(cup)
+    answer.append(change)
+    return answer
+
+
+money = 15000
+print(solution(money))
