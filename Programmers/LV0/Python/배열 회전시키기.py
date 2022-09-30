e5f43971b2ddaadf351d7074ad43f9f911e02181
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-09-30 11:30:56 +0900

    Solve: Programmers 코딩테스트 입문 배열 회전시키기

---
+++ Programmers/LV0/Python/배열 회전시키기.py
@@ -0,0 +1,16 @@
+def solution(numbers, direction):
+    answer = []
+    if direction == "right":
+        answer.append(numbers[-1])
+        for i in range(0, len(numbers)-1):
+            answer.append(numbers[i])
+    elif direction == "left":
+        for i in range(1, len(numbers)):
+            answer.append(numbers[i])
+        answer.append(numbers[0])
+    return answer
+
+
+numbers = [4, 455, 6, 4, -1, 45, 6]
+direction = "left"
+print(solution(numbers, direction))
