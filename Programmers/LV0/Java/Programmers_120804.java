f2cdac053dc4e9f913a1550fe313ee99cbbef77d
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-11-03 11:20:09 +0900

    Solve: Programmers 두 수의 곱

---
+++ Programmers/LV0/Java/Programmers_120804.java
@@ -0,0 +1,16 @@
+public class Programmers_120804 {
+    public static void main(String[] args) {
+        Programmers_120804 s = new Programmers_120804();
+
+        int num1 = 5;
+        int num2 = 4;
+
+        System.out.println(s.solution(num1, num2));
+    }
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+}
