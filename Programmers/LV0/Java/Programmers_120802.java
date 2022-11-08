4e8a467028365c2ba5fc89a27b78443fd74792a0
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-11-08 10:23:10 +0900

    Solve: Programmers 두 수의 합

---
+++ Programmers/LV0/Java/Programmers_120802.java
@@ -0,0 +1,14 @@
+public class Programmers_120802 {
+    public static void main(String[] args) {
+        Programmers_120802 s = new Programmers_120802();
+
+        int num1 = 10;
+        int num2 = 15;
+
+        System.out.println(s.solution(num1, num2));
+    }
+
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
