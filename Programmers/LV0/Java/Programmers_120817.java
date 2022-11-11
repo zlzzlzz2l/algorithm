8d56ee134ab725194663e2bb234f5e78010ca862
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-11-11 10:43:09 +0900

    Solve: Programmers 배열의 평균값

---
+++ Programmers/LV0/Java/Programmers_120817.java
@@ -0,0 +1,21 @@
+public class Programmers_120817 {
+    public static void main(String[] args) {
+        Programmers_120817 s = new Programmers_120817();
+
+        int[] numbers = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+
+        System.out.println(s.solution(numbers));
+    }
+
+    public double solution(int[] numbers) {
+        double answer = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+
+        answer = answer / numbers.length;
+
+        return answer;
+    }
+}
