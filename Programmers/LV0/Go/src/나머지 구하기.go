f6795b0e92f301028ac14ef6fa66d92456997f48
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-12-31 16:30:49 +0900

    Solve: Programmers 나머지 구하기

--- Programmers/LV0/Go/src/나머지 구하기.go
+++ Programmers/LV0/Go/src/나머지 구하기.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func solution_나머지구하기(num1 int, num2 int) int {
+func solution_120810(num1 int, num2 int) int {
 	return num1 % num2
 }
 
 func main() {
-	fmt.Println(solution_나머지구하기(15, 3))
+	fmt.Println(solution_120810(15, 3))
 }
