f22bb946f3e58ec13d67bf288873ceae06e6b47a
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-12-31 16:47:49 +0900

    Solve: Programmers 두 수의 나눗셈

--- Programmers/LV0/Go/src/두 수의 나눗셈.go
+++ Programmers/LV0/Go/src/두 수의 나눗셈.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func solution_두수의나눗셈(num1 int, num2 int) int {
+func solution_120806(num1 int, num2 int) int {
 	var result = (float64(num1) / float64(num2)) * 1000
 	return int(result)
 }
 
 func main() {
-	fmt.Println(solution_두수의나눗셈(3, 2))
+	fmt.Println(solution_120806(3, 2))
 }
