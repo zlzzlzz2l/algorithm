629ca2cc592626d61b7e530463a9a62dcada1be0
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-12-31 16:44:51 +0900

    Solve: Programmers 나이 출력

--- Programmers/LV0/Go/src/나이 출력.go
+++ Programmers/LV0/Go/src/나이 출력.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func solution_나이출력(age int) int {
+func solution_120820(age int) int {
 	return 2022 - age + 1
 }
 
 func main() {
-	fmt.Println(solution_나이출력(20))
+	fmt.Println(solution_120820(20))
 }
